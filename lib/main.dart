47f788c37a0585f4d1affa6eb4f3ec5307c9ea35
Leonardo <leonardo_custodio12@hotmail.com>
2024-07-03 22:12:43 -0300

    master

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:expenses/components/transaction_form.dart';
+import 'package:expenses/components/transaction_user.dart';
+import 'package:flutter/material.dart';
+
+import 'components/transaction_list.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          TransactionUser(),
+        ],
+      )
+    );
+  }
+}
